43ca6cb9ed185a81b365cea4f5481e107c79ea9a
Juan <juan.periscal88@gmail.com>
2019-04-04 13:22:46 +0200

    Nueva Clase CajaAceptarCerrar

---
+++ Academia2/src/GUI/CajaAceptarCerrar.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+
+public class CajaAceptarCerrar extends Box{
+	private JButton cancelar;
+	private JButton aceptar;
+
+	public CajaAceptarCerrar(Object o) {
+		super(0);
+		CajaAceptarCerrar.createHorizontalBox();
+
+
+		aceptar = new JButton();
+		cancelar= new JButton();
+
+		aceptar.addActionListener((ActionListener) o);
+		cancelar.addActionListener((ActionListener) o);
+
+		aceptar.setText("Aceptar");
+		cancelar.setText("Cerrar");
+		
+		aceptar.setBackground(Color.GRAY);
+		cancelar.setBackground(Color.GRAY);
+	}
+}
